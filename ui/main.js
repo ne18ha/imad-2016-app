295182317561a98e7c987069ba61821bb92c7679
ne18ha <ns18121997@gmail.com>
2016-10-01 00:04:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,11 @@ function moveRight(){
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
 };
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick = function(){
+counter=counter+1;
+var span=document.getElementById('count');
+span.innerHTML=counter.toString();
+};
