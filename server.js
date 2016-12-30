9ee38fdbfeb03417d18bf041797c3895d0bfc6d0
ne18ha <ns18121997@gmail.com>
2016-12-30 12:21:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -49,11 +49,6 @@ app.get('/articles/:articleName', function(req,res){
 app.get('/ims', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-var counter=0;
-app.get('/counter',function(req,res){
-   counter=counter+1;
-   res.send(counter,toString());
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
