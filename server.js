4a95942a7761bd40e6a7a83120a138cac8a489b4
Neha Sharma <ns18121997@gmail.com>
2017-01-10 19:42:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/ui/close.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'close.jpg'));
 });
 function pop(){
-    document.getElementById("display").style.display="block"
+    document.getElementById("display").style.display="block";
 }
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
